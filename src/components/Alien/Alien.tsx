03b18655e2c621365af2ce361de8e872c9949ff4
danyealmateen <danyeal.mateen@hotmail.com>
2023-09-30 09:41:14 +0200

    Alien movement implemented

--- src/components/Alien/Alien.tsx
+++ src/components/Alien/Alien.tsx
@@ -1,14 +1,34 @@
+import { useEffect, useState } from "react";
 import { AlienProps } from "../../../types";
+
 import "./Alien.css";
 
 const Alien: React.FC<AlienProps> = ({ initialPosition }) => {
+  const [position, setPosition] = useState(initialPosition);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      //här sätter man det nuvarande värdet, alltså position i staten som refereras  till prevPosition och så skapar man ett objekt där alla egenskaper av prevPosition behålls ( detta händer genom ...prevPosition ), men jag ökar värdet av x med 5.
+      setPosition((prevPosition) => ({
+        ...prevPosition,
+        // x: prevPosition.x + 5,
+        y: prevPosition.y + 5,
+      }));
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div
-      className="alien"
-      style={{ 
-        left: `${initialPosition.x}px`, 
-        top: `${initialPosition.y}px` }}
-    ></div>
+    <div className="alien-container">
+      <div
+        className="alien"
+        style={{
+          left: `${position.x}px`,
+          top: `${position.y}px`,
+        }}
+      ></div>
+    </div>
   );
 };
 export default Alien;
